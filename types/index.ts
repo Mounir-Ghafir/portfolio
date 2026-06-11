0382a4bf82ed5de40da1d95ce2839ee40043beda
ENNA-STUDENT <mounirghafirlw@gmail.com>
2026-06-11 12:27:48 +0100

    create the files and folders structure and write interfaces

---
+++ types/index.ts
@@ -0,0 +1,21 @@
+
+export interface Project {
+  title: string
+  description: string
+  stack: string[]
+  githubUrl: string
+  demoUrl: string
+}
+
+export interface Skill {
+  name: string
+  emoji: string
+}
+
+export interface Experience {
+  title: string
+  organization: string
+  period: string
+  description: string
+}
+
